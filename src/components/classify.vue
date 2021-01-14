8559dc2b76b41f137ee90b9684213db8501f4275
ziangkai <ziangkai@yeah.net>
2021-01-14 22:33:10 +0800

    完善了更多

--- src/components/classify.vue
+++ src/components/classify.vue
@@ -3,33 +3,69 @@
         <div class="top">
             <div><span>商品分类</span></div>
             <div class="search">
+                <span class="iconfont">&#58896;</span>
                 <input type="text" placeholder="年货礼盒满229-210">
             </div>
         </div>
-        <div class="other">
-            <div class="list">
-                <div>
-                    <span>热门推荐</span>
-                    <div class="sign"></div>
-                    </div>
-            </div>
+        <div class="other" >
+      
+            	<div class="list">
+                   <div>
+                        <div v-for="(item,index) in productList" :key="index" @click="cupPage(item.title)">
+					<div v-if="item.title == theTitle" class="cup">
+						<span>{{item.title}}</span>
+						<div v-if="theTitle == item.title" class="sign"></div>
+					</div>
+                    <div v-else>
+						<span>{{item.title}}</span>
+						<div v-if="theTitle == item.title" class="sign"></div>
+					</div>
+				    </div>
+                   </div>
+                </div>
+		
             <div class="main">
-                <component :is="componentName"></component>
+                <component :is="componentName" :title="theTitle" ref="child"></component>
             </div>
         </div>
+        
     </div>
 </template>
 
 <script>
 import hotTuijian from "./classify-components/hotTuijian"
 export default {
+    created(){
+        let url = "http://localhost:8080/json/rementuijian.json";
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET',url);
+        xhr.send();
+        let that = this;
+        xhr.onload = function(){
+            that.productList = JSON.parse(xhr.response);
+            // console.log(that.productList)
+            that.theTitle = that.productList[0].title;
+            // console.log(that.theTitle)
+        }
+    },
      data(){
         return {
-            componentName:"hot-tuijian"
+            productList:[],
+            componentName:"hot-tuijian",
+            theTitle:''
         }
     },
     components:{
         "hot-tuijian":hotTuijian
+    },
+    methods:{
+        cupPage(title){
+            this.theTitle = title
+            // document.querySelector(".main").scrollTop = 0
+            // this.$refs.moves.scrollTop = 0
+            // console.log(this.theTitle)
+            this.$refs.child.left = 0;
+        }
     }
 }
 </script>
@@ -50,6 +86,7 @@ export default {
         height: 100%;
         /* background-color: grey; */
         width: 27%;
+        flex-shrink: 0;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -63,7 +100,7 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-      
+       position: relative;
         padding: 1px;
 
     }
@@ -80,18 +117,34 @@ export default {
         letter-spacing: 1px;
         background-color: #f7f7f7;
     }
+    .search>span{
+        position: absolute;
+        left: 20px;
+        color: #666;
+    }
     .other{
-        
+       
         flex-grow: 1;
         display: flex;
     }
+    
     .list{
          width: 27%;
          flex-shrink: 0;
+         overflow: auto;
+         /* flex-grow: 1; */
+         display: flex;
+         flex-direction: column;
+         height: 100%;
          /* background-color: rosybrown; */
     }
-    .list>div:nth-child(1){
+    .list>div{
+        height: 0;
+    }
+    .list>div>div{
         height: 50px;
+        flex-shrink: 0;
+        /* flex-grow: 1; */
         width: 100%;
         background-color: #f7f7f7;
         display: flex;
@@ -117,5 +170,17 @@ export default {
         left: 10px;
         border-radius: 2px;
     }
+    .cup{
+        background-color: white;
+        height: 100%;
+        width: 100%;
+        display: flex;
+         display: flex;
+        justify-content: center;
+        align-items: center;
+         position: relative;
+
+    }
+    
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-
+import "./iconfont/iconfont.css"
 Vue.use(VueRouter)
 
 import ElementUI from 'element-ui';
