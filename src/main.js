44cfb6fc61d03b8e42ad04cb47a9da490e0bd394
liuchao971018 <liuchao19971018@163.com>
2021-01-16 09:18:22 +0800

    重新提交删除icont

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from "./router/index.js"
-// import "./iconfont/iconfont.css"
+// import "./iconfont2/iconfont.css"
 import "./iconfont2/iconfont.css"
 
 Vue.use(VueRouter)
 import store from "./store/index.js"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import "./iconfont2/iconfont/iconfont.css";
+import "./iconfont2/iconfont/iconfont.css";
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
